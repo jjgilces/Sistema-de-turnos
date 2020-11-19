804b07239f29111680c7090dfc0924132eb186a8
johangilces <jjgilces@espol.edu.ec>
2020-11-19 12:09:17 -0500

    Creacion de clases

--- SistemaDeTurnos/src/modelo/Medico.java
+++ SistemaDeTurnos/src/modelo/Medico.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package modelo;
 
 /**
@@ -10,5 +6,8 @@ package modelo;
  * @author user
  */
 public class Medico {
+    private String nombres;
+    private String apellidos;
+    
     
 }
--- SistemaDeTurnos/src/modelo/Paciente.java
+++ SistemaDeTurnos/src/modelo/Paciente.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package modelo;
 
 /**
@@ -10,5 +6,29 @@ package modelo;
  * @author user
  */
 public class Paciente {
-    
+    private String nombres;
+    private String apellidos;
+    private int edad;
+    private String genero;
+    private int prioridad;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
