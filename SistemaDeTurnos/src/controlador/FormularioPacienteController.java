d52eeb5cdea3676ecc3152151b5d43f34118bd00
User <hatuspellegrini1@gmail.com>
2020-11-20 12:34:10 -0500

    Agregado modelo sintoma, y lectura de archivo sintomas.txt

--- SistemaDeTurnos/src/controlador/FormularioPacienteController.java
+++ SistemaDeTurnos/src/controlador/FormularioPacienteController.java
@@ -5,9 +5,15 @@
  */
 package controlador;
 
+import java.awt.Button;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import modelo.Sintoma;
 
 /**
  * FXML Controller class
@@ -15,6 +21,23 @@ import javafx.fxml.Initializable;
  * @author User
  */
 public class FormularioPacienteController implements Initializable {
+    @FXML
+    private TextField txtNombre;
+    @FXML
+    private TextField txtApellidos;
+    @FXML
+    private TextField txtEdad;
+    @FXML
+    private RadioButton rbtMasculino;
+    @FXML
+    private RadioButton rbtFemenino;
+    @FXML
+    private ComboBox cboSintoma;
+    @FXML
+    private Button btnCancelar;
+    @FXML
+    private Button btnRegistrar;
+    
 
     /**
      * Initializes the controller class.
@@ -22,6 +45,7 @@ public class FormularioPacienteController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        cboSintoma.getItems().addAll(Sintoma.sintomas);
     }    
     
 }
