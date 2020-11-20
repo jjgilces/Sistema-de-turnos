84cd03f20deacf32861e40c938e594f358189bcc
eapb99 <emaalpar@espol.edu.ec>
2020-11-20 10:02:09 -0500

    Revert "Bug"
    This reverts commit ee6cb21fad9b517c0648c8e2cef2f2d413e5561f.

--- SistemaDeTurnos/src/modelo/Medico.java
+++ SistemaDeTurnos/src/modelo/Medico.java
@@ -8,33 +8,6 @@ package modelo;
 public class Medico {
     private String nombres;
     private String apellidos;
-
-    public Medico(String nombres, String apellidos) {
-        this.nombres = nombres;
-        this.apellidos = apellidos;
-    }
-
-    public String getNombres() {
-        return nombres;
-    }
-
-    public void setNombres(String nombres) {
-        this.nombres = nombres;
-    }
-
-    public String getApellidos() {
-        return apellidos;
-    }
-
-    public void setApellidos(String apellidos) {
-        this.apellidos = apellidos;
-    }
-
-    @Override
-    public String toString() {
-        return "Medico{" + "nombres=" + nombres + ", apellidos=" + apellidos + '}';
-    }
-    
     
     
 }
