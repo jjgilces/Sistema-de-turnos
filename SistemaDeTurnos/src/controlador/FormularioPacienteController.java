e54548f8a816e12a4f8f5dd78e8354e5ae309e98
User <hatuspellegrini1@gmail.com>
2020-11-27 14:27:49 -0500

    Creado clase BaseDatos para serilizacion de Datos. Anadido prototipado de logica detras de la asignacion por prioridad de pacientes, sujeto a cambios.

--- SistemaDeTurnos/src/controlador/FormularioPacienteController.java
+++ SistemaDeTurnos/src/controlador/FormularioPacienteController.java
@@ -5,8 +5,12 @@
  */
 package controlador;
 
+import Serializado.BaseDatos;
+import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -21,6 +25,7 @@ import javafx.stage.Stage;
 import modelo.Paciente;
 import modelo.Sintoma;
 import static sistemadeturnos.Data.sintomas;
+import sistemadeturnos.SistemaDeTurnos;
 
 /**
  * FXML Controller class
@@ -67,6 +72,16 @@ public class FormularioPacienteController implements Initializable {
 
             }
             Paciente p1 = new Paciente(txtNombre.getText(), txtApellidos.getText(), Integer.parseInt(txtEdad.getText()), genero, sintoma.getPrioridad());
+            //System.out.println(p1);
+            //Anadiendo a la lista estatica del Main
+            SistemaDeTurnos.listaPacientes.add(p1);
+            //Anadiendo y guardando en la base de datos
+            BaseDatos.listaPacientes.add(p1);
+            BaseDatos.guardarPacientes();
+            txtNombre.clear();
+            txtApellidos.clear();
+            txtEdad.clear();
+            
 
         } catch (NumberFormatException ex) {
             txtEdad.clear();
@@ -74,8 +89,7 @@ public class FormularioPacienteController implements Initializable {
         } catch (IllegalArgumentException ex) {
 
             if (!(isAlpha(txtNombre.getText()) && isAlpha(txtApellidos.getText()))) {
-                txtNombre.clear();
-                txtApellidos.clear();
+
                 mostrarAlerta("En los campos Nombre y Apellido utilice unicamente letras porfavor.", AlertType.ERROR);
             }
             if (Integer.parseInt(txtEdad.getText()) < 0) {
@@ -83,12 +97,14 @@ public class FormularioPacienteController implements Initializable {
                 mostrarAlerta("Ingrese solo numeros positivos porfavor.", AlertType.ERROR);
             }
 
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(FormularioPacienteController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
     public static void mostrarAlerta(String mensaje, AlertType e) {
         Alert alert = new Alert(e);
-        
+
         alert.setTitle("Error!");
         alert.setHeaderText("Hubo un error en el ingreso de datos!");
         alert.setContentText(mensaje);
