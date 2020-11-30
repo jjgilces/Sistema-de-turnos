552ece76407df84c3650c96e4385c03ef8ce49bb
eapb99 <emaalpar@espol.edu.ec>
2020-11-30 10:35:18 -0500

    Correct bug

--- SistemaDeTurnos/src/Serializado/Serializable.java
+++ SistemaDeTurnos/src/Serializado/Serializable.java
@@ -9,7 +9,6 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
-import sistemadeturnos.SistemaDeTurnos;
 import static sistemadeturnos.SistemaDeTurnos.medicos;
 import static sistemadeturnos.SistemaDeTurnos.pacientes;
 import static sistemadeturnos.SistemaDeTurnos.puestosAsignados;
@@ -20,8 +19,7 @@ import static sistemadeturnos.SistemaDeTurnos.puestosAsignados;
  */
 public class Serializable {
     public static void  guardarObjeto(String source, Object o){
-        try {
-            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("src/recursos/archivos/"+source));
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("src/recursos/archivos/"+source))){
             oos.writeObject(o);
             oos.flush();
         } catch (FileNotFoundException ex) {
